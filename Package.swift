be7cffa5240a321505ad940a4e7ec57665a46a9d
Julien L'Hernault <>
2022-12-23 14:44:56 +0100

    provides NetworkLoggerPlugin linked to swift-log

--- Package.swift
+++ Package.swift
@@ -16,13 +16,17 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/popina/Moya.git", .upToNextMajor(from:"13.0.2")),
+        .package(url: "https://github.com/apple/swift-log", .upToNextMajor(from: "1.4.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PopinaMoya",
-            dependencies: ["Moya"]),
+            dependencies: [
+                "Moya",
+                .product(name: "Logging", package: "swift-log")
+            ]),
         .testTarget(
             name: "PopinaMoyaTests",
             dependencies: ["PopinaMoya"]),
--- Sources/PopinaMoya/PopinaMoya.swift
+++ Sources/PopinaMoya/PopinaMoya.swift
@@ -1,3 +1,12 @@
-struct PopinaMoya {
-    var text = "Hello, World!"
+import Logging
+import Moya
+
+public class PopinaMoya {
+    public static let logger = Logger(label: "com.popina.default")
+    
+    public static func getLoggerPlugin(file: String = #fileID) -> PluginType {
+        return NetworkLoggerPlugin(verbose: true) { separator, terminator, items in
+            PopinaMoya.logger.debug("moya_\(file.components(separatedBy: "/").first!)", metadata: ["items": "\(items.map({ "\($0)" }).joined(separator: separator))"])
+        }
+    }
 }
