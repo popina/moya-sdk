f9743d872ba9c5b12280320301e367b147671232
Masto <fonkymasto@gmail.com>
2026-03-18 14:04:07 +0100

    fix moya plugin

--- Sources/PopinaMoya/PopinaMoya.swift
+++ Sources/PopinaMoya/PopinaMoya.swift
@@ -5,8 +5,6 @@ public class PopinaMoya {
     public static let logger = Logger(label: "com.popina.default")
     
     public static func getLoggerPlugin(file: String = #fileID) -> PluginType {
-        return NetworkLoggerPlugin(verbose: true) { separator, terminator, items in
-            PopinaMoya.logger.debug("moya_\(file.components(separatedBy: "/").first!)", metadata: ["items": "\(items.map({ "\($0)" }).joined(separator: separator))"])
-        }
+        return NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))
     }
 }
